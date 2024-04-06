af08210e462f6abcc93114536501f1171e42cc1d
vinhoMesquita <vinicius.albuquerque.etech@gmail.com>
2024-04-06 16:48:07 -0300

    first commit

--- mediaPeso.py
+++ mediaPeso.py
@@ -2,6 +2,19 @@
 A = float(input())
 B = float(input())
 
-media =(3.5 * A + 7.5 * B)/11
+pesoA = 3.5
+pesoB = 7.5
+
+soma_peso = pesoA + pesoB
+
+nota_pesoA = A * pesoA
+nota_pesoB = B * pesoB
+
+soma_notas = nota_pesoA + nota_pesoB
+
+
+
+media = soma_notas/ soma_peso
+
 
 print(f"MEDIA = {media:.5f}")
